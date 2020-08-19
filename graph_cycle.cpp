6f85f0e0c9435680814369fe4a95bd7de4761cf7
Lavinia Miranda <laviniameds@icloud.com>
2020-08-18 21:15:00 -0300

    graph cycle ajust

--- graph_cycle.cpp
+++ graph_cycle.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include<bits/stdc++.h> 
+#include <queue>
 using namespace std;
 
 //cria a estrututa de uma aresta com vertice origem e destino
@@ -12,7 +12,7 @@ struct edge {
 class Graph {
 public:
     //cria uma lista de adjacências
-	list<int> *list_adj;
+	vector<vector<int>> list_adj;
     vector<edge> edges;
 
     //construtor do grafo
@@ -21,7 +21,7 @@ public:
         edges = e;
 
         //define tamanho da lista
-		list_adj = new list<int>[size]; 
+		list_adj.resize(size);
 
         //para cada aresta, preenche a lista de adjacências com vertices origem e destino
 		for (auto &edge: edges)
@@ -29,16 +29,15 @@ public:
 	}
 };
 
-bool hasCycleUtil(Graph &graph, int v, bool visited[], bool *stack){
+bool hasCycleUtil(Graph &graph, int v, vector<bool> &visited, vector<bool> &stack){
     if(visited[v] == false){
         visited[v] = true; 
         stack[v] = true; 
 
-        list<int>::iterator it; 
-        for(it = graph.list_adj[v].begin(); it != graph.list_adj[v].end(); ++it) { 
-            if (!visited[*it] && hasCycleUtil(graph, *it, visited, stack)) 
+        for(int u : graph.list_adj[v]) { 
+            if (!visited[u] && hasCycleUtil(graph, u, visited, stack)) 
                 return true; 
-            else if (stack[*it]) 
+            else if (stack[u]) 
                 return true; 
         } 
     }
@@ -49,22 +48,19 @@ bool hasCycleUtil(Graph &graph, int v, bool visited[], bool *stack){
 // retorna verdadeiro/falso para se grafo tem um ciclo
 bool hasCycle(Graph &graph, int size){        
     //cria um vetor auxiliar para guardar se cada vertice foi visitado
-	bool *visited = new bool[size]; 
-	bool *stack = new bool[size]; 
-
-	for(int i = 0; i < size; i++) { 
-		visited[i] = false; 
-		stack[i] = false; 
-	} 
+    vector<bool> visited(size);
+    vector<bool> stack(size);
   
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++){
         if (hasCycleUtil(graph, i, visited, stack))
-            return true;       
+            return true;    
+    }
     
     return false;
 }
 
-int main(){
+int main()
+{
 	vector<edge> edges = {
 		{1, 4}, {4,2}, {4,3}, {4, 5}, {5, 6}
 	};
